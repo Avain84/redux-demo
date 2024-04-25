fa8742d3258ef5a20e6caccf96d60799d9ffe9e5
Avain <sports113499@gmail.com>
2024-04-25 15:25:09 +0800

    feat:add Provider and store in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { Provider } from 'react-redux';
+import store from '../redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* 專案進入點使用redux包住 */}
+    {/* 讓所有元件能夠與資料庫取得聯繫 */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
